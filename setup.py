9cc549839fa276d8ad96a0f195a734eb26034860
Ramya <n160696@rguktn.ac.in>
2024-07-21 02:27:47 +0530

    Completed_code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Ramya',
+    author_email='n160696@rguktn.ac.in',
+    packages=find_packages(),
+    install_requires=[]
+)
